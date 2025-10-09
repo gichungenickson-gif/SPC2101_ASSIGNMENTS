2315f672334a57173fe150aa3172baaa5f7c6299
gichungenickson-gif <gichungenickson@gmail.com>
2025-10-09 20:12:05 +0300

    Add files via upload

---
+++ print_numbers.c
@@ -0,0 +1,17 @@
+/*
+Name :gichunge nickson mutugi
+Reg no:CT101/G/26562/25
+DATE:9 SEP 2025
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
